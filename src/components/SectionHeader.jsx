ed9167e0cd5d5c063c700fcc2e8f615d18d54454
Kai Yuan Neo <kai@rocketacademy.co>
2019-12-11 13:34:15 +0800

    clean up imports and remove unnecessary css class

--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 
 const SectionHeader = props => (
-  <Container className="d-flex space-between section-header">
+  <Container className="d-flex section-header">
     <span className="section-header-title">{props.headerText}</span>
     <span className="section-header-subtitle">
       {new Date().toDateString()} | {props.minTemp} / {props.maxTemp} | ST Index{" "}
